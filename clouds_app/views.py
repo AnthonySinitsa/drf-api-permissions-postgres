28361cc634e4dac7b27812f6af049c42310142c9
AnthonySinitsa <itonyemail@gmail.com>
2023-07-25 22:06:39 +0000

    command

--- clouds_app/views.py
+++ clouds_app/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
 from .serializer import CloudSerializer
 from .models import Cloud
+from .permissions import IsOwnerOrReadOnly
 
 class CloudList(generics.ListCreateAPIView):
+  permission_classes = (IsOwnerOrReadOnly)
   queryset = Cloud.objects.all()
   serializer_class = CloudSerializer
   
 class CloudDetail(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes = (IsOwnerOrReadOnly)
   queryset = Cloud.objects.all()
   serializer_class = CloudSerializer
--- clouds_project/urls.py
+++ clouds_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/clouds/', include('clouds_app.urls')),
+    path('api-auth/', include('rest_framework.urls')), # this add login/logout to the browsable API
 ]
